1c8cb8f1ef582b018fbc821eb3d2e7b12e10840b
Sangmin <tkdals730@naver.com>
2026-01-27 17:41:25 +0900

    fornum코드 변수 활용

--- pratice1-6.c
+++ pratice1-6.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 
-// "#### ����
-// �� ĭ�� ���Ե� ���ڿ� ""Be happy!""�� ������ ���� ���� char�� �迭�� �����ϰ� Ű����κ��� �Է��ϴ� �ڵ带 �ۼ��մϴ�. "
+// "#### 문제
+// 빈 칸이 포함된 문자열 ""Be happy!""를 저장할 가장 작은 char형 배열을 선언하고 키보드로부터 입력하는 코드를 작성합니다. "
 
 int main(void) {
 
 	char happy[10];
-	printf("���ڸ� �Է��� �ּ��� : ");
+	printf("문자를 입력해 주세여 : ");
 	gets(happy);
-	printf("�Է¹��� ���ڿ� : ");
+	printf("입력받은 문자열 : ");
 	puts(happy);
 
 	return 0;
