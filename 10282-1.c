878bdb18da7a6a99ca442c1a3a741b51adb70922
Sangmin <tkdals730@naver.com>
2026-01-28 11:40:08 +0900

    중복이 있을경우 문제발생,입력값이 음수일떄 문제

---
+++ 10282-1.c
@@ -0,0 +1,68 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+/*
+"#### 문제
+다섯 명의 심사위원 점수를 입력하여 유효점수와 평균을 출력하는 프로그램을 작성합니다.유효점수는 최고점과 최저점을 제외한 점수 이며 평균은 유효점수로 계산합니다.
+
+#### 실행결과
+5명 심사위원의 점수 입력 : 7 9 4 8 5
+유효점수 : 7 8 5
+평균 : 6.7 "
+*/
+int max_value(int score[], int size);
+int min_value(int score[], int size);
+int main(void) {
+
+	int scores[5];
+	double avg;
+	int total = 0;
+	int size = sizeof(scores) / sizeof(scores[0]);
+	printf("5명의 심사위원의 점수 입력 : ");
+	for (int i = 0; i < size ; i++) {
+		scanf("%d", &scores[i]);
+		total = total + scores[i];
+	}
+	int max = max_value(scores,size);
+	int min = min_value(scores,size);
+	total = total - max - min ;
+	avg = (double) total / (size - 2);
+
+	printf("유효 점수 : ");
+	for (int i = 0; i < size; i++) {
+		if (max > scores[i] && scores[i] > min) {
+			printf("%d ", scores[i]);
+		}
+		
+	}
+	
+	printf("\n평균 : %.1lf\n", avg);
+	return 0;
+}
+
+int max_value(int score[], int size) {
+	
+	int max = 0;
+	for (int i = 0; i < size; i++) {
+		if (max < score[i]) {
+			max = score[i];
+		}
+	}
+	return max;
+}
+int min_value(int score[], int size) {
+
+	int min = 0;
+	int count = 0;
+	for (int i = 0; i < size ; i++) {
+		if (count == 0) {
+			min = score[0];
+			count++;
+		}
+		if (min > score[i]) {
+			min = score[i];
+		}
+		
+	}
+	return min;
+}
