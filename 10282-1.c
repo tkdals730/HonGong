5d56b0ef8d872ff92ab302e67866a120d7e01896
Sangmin <tkdals730@naver.com>
2026-01-28 11:59:57 +0900

    이해안되는 부분 주석추가해서 이해해봄 쉬운 방법이 있는것같다

--- 10282-1.c
+++ 10282-1.c
@@ -10,7 +10,17 @@
 유효점수 : 7 8 5
 평균 : 6.7 "
 */
+
+// 배열을 함수에 넘기면 배열자체의 크기가 사라진다.
+// 그래서 배열의 크기를 따로 같이 넘겨야한다.
 int max_value(int score[], int size);
+// 이거를 함수에 넘길떄 컴파일러에서
+// max_value(&scores[0], size); 이렇게 인식한데
+// 그러니까 주소만 넘어간데 
+// 포인터는 크기는 모두같고 시작하는 위치만 저장하잖음
+// 그럼 언제까지인지는 모르잖아
+// 그래서 문제가 생긴거다.
+
 int min_value(int score[], int size);
 int main(void) {
 
@@ -29,6 +39,8 @@ int main(void) {
 	avg = (double) total / (size - 2);
 
 	printf("유효 점수 : ");
+	
+	// 약간 불리언같이 쓰는 느낌으로 받아들이면 될듯
 	int remove_max = 0;
 	int remove_min = 0;
 	for (int i = 0; i < size; i++) {
