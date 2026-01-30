ca243671b26d625dd4ea1cd0a0b709c131ddd71b
Sangmin <tkdals730@naver.com>
2026-01-30 11:10:35 +0900

    카드 잔액이 부족할경우 처리

--- 0130.c
+++ 0130.c
@@ -5,12 +5,18 @@ int main(void) {
 
 	//자판기 번호?
 	while (1) {
+		int cola_price = 1500;
+		int cider_price = 1500;
+		int fanta_price = 1200;
+		int yuja_tea__price = 1700;
 
 		int cola_count = 4;
 		int cider_count = 3;
 		int fanta_count = 2;
 		int yuja_tea_count = 0;
 		int select_num;
+
+		int account = 10000;
 
 		printf("상품목록\n");
 		printf("1. 콜라\n");
@@ -36,8 +42,30 @@ int main(void) {
 			printf("재고가 없습니다. 다시 선택해주세요\n");
 			continue;
 		}
+		else if (select_num > 4) {
+			printf("번호를 다시 입력해주세여\n");
+			continue;
+		}
 
-		break;
+		printf("카드를 넣어주세요\n");
+		
+		if (account < cola_price && select_num == 1) {
+			printf("잔액이 부족합니다.\n");
+			continue;
+		}
+		else if (account < cider_price && select_num == 2) {
+			printf("잔액이 부족합니다.\n");
+			continue;
+		}
+		else if (account < fanta_price && select_num == 3) {
+			printf("잔액이 부족합니다.\n");
+			continue;
+		}
+		else if (account < yuja_tea__price && select_num == 4) {
+			printf("잔액이 부족합니다.\n");
+			continue;
+		}
+		
 
 	}
 
